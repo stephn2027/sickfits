0e0e39fa192bf758cd59779f5e58be6dd228d6f6
stephen <stephn2027@gmail.com>
2022-03-29 11:24:22 +0900

    configured keystone and connected elephantQL

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -14,13 +14,15 @@ import { lists } from './schema';
 // Keystone auth is configured separately - check out the basic auth setup we are importing from our auth file.
 import { withAuth, session } from './auth';
 
+const databaseURL = process.env.DATABASE_URL;
+const frontendURL = process.env.FRONTEND_URL;
 export default withAuth(
   // Using the config function helps typescript guide you to the available options.
   config({
     // the db sets the database provider - we're using sqlite for the fastest startup experience
     db: {
-      provider: 'sqlite',
-      url: 'file:./keystone.db',
+      provider: 'postgresql' || 'sqlite',
+      url: databaseURL || 'file:./keystone.db',
     },
     // This config allows us to set up features of the Admin UI https://keystonejs.com/docs/apis/config#ui
     ui: {
@@ -29,5 +31,11 @@ export default withAuth(
     },
     lists,
     session,
+    server: {
+      cors: {
+        origin: frontendURL,
+        credentials: true,
+      },
+    },
   })
 );
