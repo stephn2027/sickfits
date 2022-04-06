ce26282a853e2d2064c11d5645101b0bf8cb11b5
stephen <stephn2027@gmail.com>
2022-04-06 13:16:45 +0900

    changed adapter of keystone to primsa

--- backend/seed-data/index.ts
+++ backend/seed-data/index.ts
@@ -1,19 +1,23 @@
+
+
+
+import keystone from '../keystone';
 import { products } from './data';
 
-export async function insertSeedData(ks: any) {
+export async function insertSeedData(keystone: any) {
   // Keystone API changed, so we need to check for both versions to get keystone
-  const keystone = ks.keystone || ks;
-  const adapter = keystone.adapters?.MongooseAdapter || keystone.adapter;
+  
+ 
 
   console.log(`🌱 Inserting Seed Data: ${products.length} Products`);
-  const { mongoose } = adapter;
+  const {postgresql} = keystone;
   for (const product of products) {
     console.log(`  🛍️ Adding Product: ${product.name}`);
-    const { _id } = await mongoose
+    const { _id } = await postgresql
     .model('ProductImage')
     .create({ image: product.photo, altText: product.description });
     product.photo = _id;
-    await mongoose.model('Product').create(product);
+    await postgresql.model('Product').create(product);
   }
   console.log(`✅ Seed Data Inserted: ${products.length} Products`);
   console.log(`👋 Please start the process with \`yarn dev\` or \`npm run dev\``);
