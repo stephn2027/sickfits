ba308df4fac20cb67696d259ce601704df86add3
stephen <stephn2027@gmail.com>
2022-04-22 12:28:36 +0900

    modified frontend webpack

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "733a9f8566e736c4477b"; }
+/******/ 		__webpack_require__.h = function() { return "4060e8551a0a4f66b7c5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
