2ffb2d48aa9aca617e38fa9a662af87e1a027b82
stephen <stephn2027@gmail.com>
2022-06-18 09:09:46 +0900

    fixed pathname of link to product after creating

--- backend/.keystone/admin/.next/static/chunks/webpack.js
+++ backend/.keystone/admin/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1da8a2b2f2795fd1a4d2";
+/******/ 	var hotCurrentHash = "71e468e4ef37e3033639";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
